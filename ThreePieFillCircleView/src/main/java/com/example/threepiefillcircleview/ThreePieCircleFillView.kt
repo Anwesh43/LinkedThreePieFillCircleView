f71a9215a964c6574baab7b4f5386900c3ee2549
Anwesh43 <anweshthecool0@gmail.com>
2021-03-24 00:44:55 +0530

    adding Canvas extension methods

--- ThreePieFillCircleView/src/main/java/com/example/threepiefillcircleview/ThreePieCircleFillView.kt
+++ ThreePieFillCircleView/src/main/java/com/example/threepiefillcircleview/ThreePieCircleFillView.kt
@@ -29,3 +29,40 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawThreePieCircleFill(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(180f * sf3)
+    paint.style = Paint.Style.STROKE
+    drawArc(
+        RectF(-size, -size, size, size),
+        0f,
+        270f * sf1,
+        false,
+        paint
+    )
+    paint.style = Paint.Style.FILL
+    drawArc(
+        RectF(-size, -size, size, size),
+        0f,
+        270f * sf2,
+        true,
+        paint
+    )
+    restore()
+}
+
+fun Canvas.drawTPCFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawThreePieCircleFill(scale, w, h, paint)
+}
