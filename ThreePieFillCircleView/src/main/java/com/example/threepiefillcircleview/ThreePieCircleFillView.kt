f28851adc058961713bb7b853f0c636d15868bd5
Anwesh43 <anweshthecool0@gmail.com>
2021-03-24 00:49:44 +0530

    adding ThreePieCircleFillView class

--- ThreePieFillCircleView/src/main/java/com/example/threepiefillcircleview/ThreePieCircleFillView.kt
+++ ThreePieFillCircleView/src/main/java/com/example/threepiefillcircleview/ThreePieCircleFillView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawTPCFNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawThreePieCircleFill(scale, w, h, paint)
 }
+
+class ThreePieCircleFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
